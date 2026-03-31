b4719fe9ff53b58b1c8037ac96d63adaa6d457ab
prozolic <42107886+prozolic@users.noreply.github.com>
2026-03-31 09:13:11 +0900

    Remove count == 0 and index == 0

--- src/libraries/System.Collections.Immutable/src/System/Collections/Immutable/ImmutableList_1.Node.cs
+++ src/libraries/System.Collections.Immutable/src/System/Collections/Immutable/ImmutableList_1.Node.cs
@@ -759,11 +759,6 @@ namespace System.Collections.Immutable
             /// </returns>
             internal int IndexOf(T item, int index, int count, IEqualityComparer<T>? equalityComparer)
             {
-                if (count == 0 && index == 0)
-                {
-                    return -1;
-                }
-
                 Requires.Range(index >= 0 && index <= this.Count, nameof(index));
                 Requires.Range(count >= 0 && count <= this.Count, nameof(count));
                 Requires.Range(index + count <= this.Count, nameof(count));
